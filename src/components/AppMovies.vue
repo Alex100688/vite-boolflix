8d739ad2c57b5fc908c803de9cf55432de1c95bf
Alex100688 <alessiozingaro2@gmail.com>
2022-12-02 16:05:44 +0100

    Aggiunge la bandiera della lingua originale.

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,9 +1,23 @@
 <script>
+import CountryFlag from 'vue-country-flag-next';
 export default
     {
-        name: "AppMovies", 
+        name: "AppMovies",
+        components: {
+            CountryFlag,
+        }, 
         props: {
             info:Object,
+        },
+       methods: {
+        dataFlag(language) {
+            if (language == 'en') {
+                return 'gb';
+            } else {
+                return language;
+            }               
+               
+        }
        },
     }
 </script>
@@ -11,7 +25,7 @@ export default
     <div>
         <h4>{{ info.title}}</h4>
         <h3>{{ info.original_title}}</h3>
-        <div>{{info.original_language}}</div>
+        <CountryFlag :country="dataFlag(info.original_language)" size="bold"/>
         <div>{{info.vote_average}}</div>
         
     </div>    
--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import CountryFlag from 'vue-country-flag-next';
-new Vue({
-    components: {
-        CountryFlag,
-    },
-})
 createApp(App).mount('#app')
